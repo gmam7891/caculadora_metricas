8963870468761bbf0705cf9ccfff01cca758fc02
gmam7891 <guilhermemontanari@gmail.com>
2026-02-11 15:45:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -115,12 +115,12 @@ with tabs[0]:
         st.markdown("### Instagram Reels")
         reels_qty = st.number_input("Qtd Reels", min_value=0, value=2, step=1)
         reels_avg_views = st.number_input("Views médias por Reel", min_value=0.0, value=0.0, step=1000.0, format="%.0f")
-        reels_ctr_pct = st.number_input("CTR Reels (%)", min_value=0.0, max_value=100.0, value=0, step=0.1)
+        reels_ctr_pct = st.number_input("CTR Reels (%)", min_value=0.0, max_value=100.0, value=0.3, step=0.1)
         reels_ctr = reels_ctr_pct / 100.0
         st.markdown("### Instagram Stories")
         stories_qty = st.number_input("Qtd Stories (frames/combos)", min_value=0, value=0, step=1)
-        stories_avg_views = st.number_input("Views médias por Story", min_value=0.0, value=0, step=1000.0)
-        stories_ctr_pct = st.number_input("CTR Stories (%)", min_value=0.0, max_value=100.0, value=0, step=0.1)
+        stories_avg_views = st.number_input("Views médias por Story", min_value=0.0, value=40000.0, step=1000.0)
+        stories_ctr_pct = st.number_input("CTR Stories (%)", min_value=0.0, max_value=100.0, value=1.0, step=0.1)
         stories_ctr = stories_ctr_pct / 100.0
 
         st.markdown("### TikTok")
